876fae0eaed07cd2cff5a29e123008f36253517c
Denis Belokopytov <tomcreo@mail.ru>
2022-12-27 17:55:50 +0300

    Решил задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Задайте длинну массива!");
+int length = Convert.ToInt32(Console.ReadLine());
+
+ string [] FillArray (int size)
+ {
+    string [] array = new string [size];
+  for (int i = 0; i < size; i++)
+  {
+    Console.WriteLine($"Введите {i} значение:");
+    array[i] = Console.ReadLine();
+  }
+  return array;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length -1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine($"]");
+}
+
+int FindValues (string [] array)
+{
+    int count = 0;
+    string arrayValue = "0";
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayValue = array[i];
+        if (arrayValue.Length <= 3) count++;
+    }
+    return count;
+}
+
+string [] ResultArray (int size, string [] array)
+{
+    string [] NewArray = new string [size];
+    string arrayValue = "0";
+    int k = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayValue = array[i];
+        if (arrayValue.Length <= 3) 
+        {
+            NewArray[k] = array[i]; 
+            k++;
+        }
+    }
+    return NewArray;
+}
+
+string [] ourArray = FillArray(length);
+PrintArray(ourArray);
+int resultValue = FindValues(ourArray);
+string [] resultArray = ResultArray(resultValue, ourArray);
+PrintArray(resultArray);
